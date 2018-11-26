7f679258854d406960682be5c1796dcbe7de7165
flaviomedeiros352 <39629830+flaviomedeiros352@users.noreply.github.com>
2018-11-26 17:47:28 -0200

    Atualizando Arquivos do Curso
    Atualizando Arquivos do Curso 2018-11-26 às 17:47

--- Projeto/index.js
+++ Projeto/index.js
@@ -1,4 +1,4 @@
-console.log("\n\n");
+console.log("\n");
 console.log("FORMAS DE DECLARAR VARIAVEIS");
 //let - Usado para declarar a variavel, serve para que a variavel
 //      exista somente no bloco que a mesma for declarada, o proc. saindo do
@@ -13,7 +13,7 @@ console.log(constOlaMundo);
 
 //##################
 //Tipos de iguais
-console.log("\n\n");
+console.log("\n");
 console.log("TIPOS DE IGUAIS");
 let a = 10;
 const b = "10";
@@ -26,8 +26,8 @@ console.log("6", a == b || typeof a == 'string');//operador OU
 
 //###############
 // IF
-console.log("\n\n");
-console.log("IF");
+console.log("\n");
+console.log("IF - SINALEIRO");
 let cor = "verde";
 if (cor == "verde"){
     console.log("siga");
@@ -38,9 +38,47 @@ if (cor == "verde"){
 }
 
 //#############
-console.log("\n\n");
-console.log("SWITCH - INSERIR CÓDIGO AQUI");
+console.log("\n");
+console.log("SWITCH - SINALEIRO");
 
+switch (cor) {
+    case "verde":
+        console.log("Siga");
+        break;
+    case "amarelo":
+        console.log("Atenção");
+        break;    
+    case "vermelho":
+        console.log("Pare");
+        break;
+}
+
+//#############
+console.log("\n");
+console.log("FOR COM BREAK");
+let n = 5;
+for(let i = 0; i < 10; i++){
+    console.log(`${i} X ${n} = ${i*n}`);
+    if(i*n > 20){
+        console.log(`${i*n} é maior que 20 então pare com break!`);
+        break;
+    }
+};
+
+//#############
+console.log("\n");
+console.log("FUNÇÕES - FUNCTION");
+function calc(x1, x2, operator){
+    return eval(`${x1} ${operator} ${x2}`);
+}
+let resultadoSoma = calc(10,10,'+');
+console.log(`Resultado da soma com function com nome: ${resultadoSoma}`);
+
+console.log(`Resultado da soma com function anónima: ${
+    (function(x1, x2, operator){
+        return eval(`${x1} ${operator} ${x2}`);
+    })(1,2,'+')
+}`);
 
 //############# 2018-11-25 - 21:30
 console.log("\n\n");
